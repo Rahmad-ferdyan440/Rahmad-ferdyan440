0094402a9a154da2dfa0d4e65bb48014e5f931e0
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-11-04 20:40:11 +0700

    v1

--- Latihan1.java
+++ Latihan1.java
@@ -1,4 +1,4 @@
-//Membuat segitiga siku-siku dengan simbol *//
+//Membuat segitiga dengan simbol *//
 
 import java.util.Scanner;
 public class Latihan1 {
@@ -6,23 +6,66 @@ public class Latihan1 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    int menu;
     int baris, jmL=0;
+    char repeat;
 
-    System.out.print("Masukkan jumlah baris: ");
-    baris = sc.nextInt();
+do{   
+    System.out.println("Pilih menu: ");
+    System.out.println("1. Segitiga siku2");
+    System.out.println("2. Segitiga piramida");
+    System.out.print("Masukkan pilihan: ");
+    menu = sc.nextInt();
 
-    for (int i=1; i<=baris; i++){
-        System.out.println(" ");
-        for(int j=1; j<=i; j++){
-            System.out.print("* ");
+    switch(menu){ 
+    
+    case 1:
+        System.out.print("Masukkan jumlah baris: ");
+        baris = sc.nextInt();
+
+        for (int i=1; i<=baris; i++){
+            System.out.println(" ");
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
         }
-    }
+        for (int k=1; k<=baris; k++){
+            jmL+=k;
+        }
+        System.out.println("");
+        System.out.printf("\nJumlah bintang (*) = %d buah", jmL);
+        break;
 
-    for (int k=1; k<=baris; k++){
-        jmL+=k;
-    }
-    System.out.println("");
-    System.out.printf("\nJumlah bintang (*) = %d buah", jmL);
+    case 2:
+        System.out.print("Masukkan jumlah baris: ");
+        baris = sc.nextInt();
+        System.out.println();
 
-    }
+        for (int i=1; i<=baris; i++){
+            for(int j=1; j<=(baris-i); j++){
+                System.out.print(" ");
+            }
+            for(int p=1; p<=i; p++){
+                    System.out.print("* ");
+                }
+          System.out.println(" ");
+        }
+
+        for (int k=1; k<=baris; k++){
+            jmL+=k;
+        }
+        System.out.println("");
+        System.out.printf("\nJumlah bintang (*) = %d buah", jmL);
+        break;
+
+    } 
+
+System.out.print("\nPilih menu lain? (y/n): ");
+repeat = sc.next().charAt(0);
+
+} while (repeat == 'y');
+
+
 }
+}
+
